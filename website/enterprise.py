02fd335054cf7313c94960efb378ccdcbb7c686b
tzuting98914 <bee87914@gmail.com>
2022-04-13 01:51:25 +0800

    事業單位新增刪除修改

--- website/enterprise.py
+++ website/enterprise.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from turtle import update
+from flask import Blueprint, render_template, request, flash, redirect,url_for
 from flask_login import current_user,login_required
 from website import connectDB
+import re
 
 connection = connectDB()
 cursor = connection.cursor()
@@ -8,27 +10,48 @@ cursor = connection.cursor()
 enterprise = Blueprint('enterprise', __name__)
 
 def getIndustry():
-	sql = 'SELECT DISTINCT * FROM INDUSTRY ORDER BY CATEGORY'
+	sql = 'SELECT DISTINCT * FROM INDUSTRY ORDER BY INDUSTRYNAME'
 	cursor.execute(sql)
 	industryData = cursor.fetchall()
 	industry_list = []
 	for data in industryData:
 		industry = {
-			'iid':data[0],
+			'inId':data[0],
 			'industryName':data[2]
 		}
 		industry_list.append(industry)
 	return industry_list
 
 def getEnterprise():
-	sql = 'SELECT * FROM ENTERPRISE NATURAL JOIN INDUSTRY WHERE ENTERPRISENO IS NOT NULL'
-	
+	# sql = 'SELECT * FROM ENTERPRISE NATURAL JOIN INDUSTRY WHERE ENTERPRISENO IS NOT NULL ORDER BY EID DESC'
+	sql = 'SELECT * FROM ENTERPRISE NATURAL JOIN INDUSTRY ORDER BY EID DESC'
 	cursor.execute(sql)
 	allEnterprise = cursor.fetchall()
-	# print(current_user.role)
+	return allEnterprise
+
+def getProject():
+	sql = 'SELECT * FROM POJECT NATURAL JOIN ENTERPRISE WHERE ROWNUM <= 10'
+	cursor.execute(sql)
+	allproject = cursor.fetchall()
+	
+	project_list = []
+	count = 0
+	for data in allproject:
+		count = count+1
+		project = {
+			'序號':count,
+            '工程名稱': data[2],
+			'業主名稱': data[8]
+        }
+		project_list.append(project)
+	return project_list
+
+	
+
+def enterprise_to_list(TotalData):
 	Enterprise_list = []
 	count = 0
-	for data in allEnterprise:
+	for data in TotalData:
 		count = count+1
 		enterprise = {
 			'序號':count,
@@ -43,49 +66,145 @@ def getEnterprise():
 		Enterprise_list.append(enterprise)
 	return Enterprise_list
 
+def get_search_term(term):
+	# 字串轉換成 %term% 格式
+	if term!= None:
+		search_term = '%' + term + '%'
+	else:
+		search_term = "%%"
+	return search_term
 
 @enterprise.route('/enterprise')
 def showEnterprise():
-	enterpriseData = getEnterprise()
+	enterpriseData = enterprise_to_list(getEnterprise())
 	industryData = getIndustry()
 	# print(industryData)
 
-	print(len(enterpriseData))
+	# print(len(enterpriseData))
 	return render_template("enterprise.html",enterpriseData = enterpriseData[0:10],industryData = industryData, user=current_user)
 
 
 @enterprise.route('/enterprise/search', methods=['GET','POST'])
 def search():
-	eId_search = request.form.get('eId_search')
-	eNo_search = request.form.get('eNo_search')
-	eName_search = request.form.get('eName_search')
+	industryData = getIndustry()
+
+	eId_search = get_search_term(request.form.get('eId_search'))
+	eNo_search = get_search_term(request.form.get('eNo_search'))
+	eName_search = get_search_term(request.form.get('eName_search'))
 	eCap_search = request.form.get('eCap_search')
-	ePr_search = request.form.get('ePr_search')
-	eAdd_search = request.form.get('eAdd_search')
+	eCap_search = int(eCap_search) if len(eCap_search) != 0 else 0
+	ePr_search = get_search_term(request.form.get('ePr_search'))
+	eAdd_search = get_search_term(request.form.get('eAdd_search'))
+	inId_search = get_search_term(request.form.get('inName_search'))
 
+	# 不搜尋資本額
+	if eCap_search==0:
+		sql = """SELECT *
+				FROM ENTERPRISE NATURAL JOIN INDUSTRY 
+				WHERE  
+					EID LIKE (:eId_search) AND
+					ENTERPRISENO LIKE (:eNo_search) AND
+					PRINCIPAL LIKE (:ePr_search) AND
+					ADDRESS LIKE (:eAdd_search) AND
+					ENTERPRISENAME LIKE (:eName_search) AND
+					INDUSTRYNAME LIKE (:inId_search) AND
+					CAPITAL >= (:eCap_search)
+				ORDER BY ENTERPRISE.EID FETCH FIRST 10 ROWS ONLY """
+	# 搜尋資本額
+	else:
+		sql = """SELECT *
+			FROM ENTERPRISE NATURAL JOIN INDUSTRY 
+			WHERE  
+				EID LIKE (:eId_search) AND
+				ENTERPRISENO LIKE (:eNo_search) AND
+				PRINCIPAL LIKE (:ePr_search) AND
+				ADDRESS LIKE (:eAdd_search) AND
+				ENTERPRISENAME LIKE (:eName_search) AND
+				INDUSTRYNAME LIKE (:inId_search) AND
+				CAPITAL = (:eCap_search)
+			ORDER BY ENTERPRISE.EID FETCH FIRST 10 ROWS ONLY """
+	# print(sql)
+	cursor.prepare(sql)
+	cursor.execute(None, {'eId_search': eId_search, 'eNo_search':eNo_search,'eName_search':eName_search, 'ePr_search': ePr_search,
+						  'eAdd_search': eAdd_search,'inId_search':inId_search,'eCap_search':eCap_search})
+	
+	allEnterprise = cursor.fetchall()
+	Enterprise_list = enterprise_to_list(allEnterprise)
+	
+	return render_template("enterprise.html",enterpriseData = Enterprise_list,industryData = industryData,user=current_user)
+	# return render_template("enterprise.html",user=current_user)
 
+@enterprise.route('/enterprise/update_or_new', methods=['GET','POST'])
+def update_or_new():
+	industryData = getIndustry()
+	actionType = request.values.get('actionType')
+	# 新增資料
+	if actionType =='add':
+		# 系統生成新的eid
+		cursor.execute('SELECT EID FROM ENTERPRISE WHERE ROWNUM =1 ORDER BY EID DESC')
+		lastEnterprise = cursor.fetchone()
+		lastEid = lastEnterprise[0]
+		newEid = str(int(re.search('\d+',lastEid).group(0)) + 1).zfill(10)		
+		# print(lastEid)
+		if request.method == 'POST':
+			# request得到的data
+			new_eid = request.values.get('newEid')
+			new_eNo = request.values.get('eNo')
+			new_inId = request.values.get('inId')
+			new_eName = request.values.get('eName')
+			new_eCap = request.values.get('eCap')
+			new_ePri = request.values.get('ePri')
+			new_eAdd = request.values.get('eAddress')
 
+			# 新增到資料庫中
+			cursor.prepare("INSERT INTO ENTERPRISE VALUES (:new_eid, :new_eNo, :new_eCap, :new_ePri, :new_eAdd, :new_inId, :new_eName)")
+			cursor.execute(None, {'new_eid':new_eid, 'new_eNo':new_eNo, 'new_eCap':new_eCap, 'new_ePri':new_ePri, 'new_eAdd':new_eAdd, 'new_inId':new_inId, 'new_eName':new_eName})
+			connection.commit()
+			
+			return redirect(url_for('enterprise.showEnterprise'))
+		return render_template("enterprise_new.html",newEid = newEid,industryData = industryData, user = current_user)
+	# 修改完畢
+	elif(actionType =='update_finish'):
+		eid = request.values.get('up_Eid')
+		# request得到的data
+		up_eNo = request.values.get('eNo')
+		up_inId = request.values.get('inId')
+		up_eName = request.values.get('eName')
+		up_eCap = request.values.get('eCap')
+		up_ePri = request.values.get('ePri')
+		up_eAdd = request.values.get('eAddress')
+		print(up_eCap)
+		cursor.prepare("""UPDATE ENTERPRISE 
+						  SET 
+						  	ENTERPRISENO=:up_eNo,
+						  	CAPITAL=:up_eCap,
+							PRINCIPAL=:up_ePri,
+							ADDRESS=:up_eAdd,
+							INID=:up_inId,
+							ENTERPRISENAME=:up_eName
+						  WHERE EID=:eid
+						""")
+		cursor.execute(None, {'eid':eid,'up_eNo':up_eNo,'up_inId':up_inId,'up_eName':up_eName,'up_eCap':up_eCap,'up_ePri':up_ePri,'up_eAdd':up_eAdd})
+		connection.commit()
+		return redirect(url_for('enterprise.showEnterprise'))
+	# 刪除資料
+	elif(request.values.get('delete')):
+		eid = request.values.get('delete')
+		cursor.prepare('DELETE FROM ENTERPRISE WHERE EID=:eid')
+		cursor.execute(None, {'eid':eid})
+		# print('delete'+eid)
+		connection.commit()
 
-@enterprise.route('/enterprise/update', methods=['GET','POST'])
-def update():
-	# request得到的data
-	eid = request.values.get('eid')
-	# print(eid)
-	# sql = 'UPDATE ENTERPRISE SET '
-	# cursor.execute(sql)
-
-	return render_template("home.html", user=current_user)
-
-@enterprise.route('/enterprise/delect')
-def delect():
-	return render_template("home.html", user=current_user)
-
-@enterprise.route('/enterprise/new', methods=['GET','POST'])
-@login_required
-def new():
-	# request得到的data
-	pid = request.values.get('pid')
-	# print(eid)
-	# sql = 'UPDATE ENTERPRISE SET '
-	# cursor.execute(sql)
-	return render_template("enterprise_new.html", user = current_user)
+		return redirect(url_for('enterprise.showEnterprise'))
+	# 回傳的是編號就是修改某個編號的資料
+	elif(request.values.get('edit')):
+		eid = request.values.get('edit')
+		print(eid)
+		cursor.prepare("SELECT * FROM ENTERPRISE NATURAL JOIN INDUSTRY WHERE EID=(:eid)")
+		cursor.execute(None, {'eid':eid})
+		connection.commit()
+		update_enterprise = cursor.fetchone()
+		Enterprise_list = list(update_enterprise)
+	else:
+		return redirect(url_for('enterprise.showEnterprise'))
+	return render_template("enterprise_new.html",Enterprisedata = Enterprise_list,industryData = industryData, user = current_user)
